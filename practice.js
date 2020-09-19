156466c3ba072fbd67b8c42e8996d786815ceaa3
Anas-i-okasha <okasha.i.anas@gmail.com>
2020-09-19 21:14:54 +0300

    fimish html and css

--- practice.js
+++ practice.js
@@ -1,11 +1,10 @@
 
-ArrayObj=[
+const ArrayObj=[
 
-    {'name:watermelon'  , 'price:1.35JD/Kg' , image:'download.jpg'}
+    {'name':'watermelon'  , 'price':'1.35JD/Kg' , 'image':'download.jpg'} , 
+    {'name':'strawberries'  , 'price':'0.85JD/Kg' , 'image':'download.jpg'} , 
+    {'name':'apple'  , 'price':'1.89JD/Kg' , 'image':'download.jpg'}, 
 ]
 
-
-
-
-
 const p = document.querySelector('div')
+p.append(ArrayObj)
